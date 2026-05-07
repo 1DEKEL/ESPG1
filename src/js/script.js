83bad6fe948f50041f6aa994c6702c4f37ae3f3b
kelvyn40 <kelvynlukalunqueira@gmail.com>
2026-05-07 11:46:24 -0300

    feat: Criando menu hamburguer

--- src/js/script.js
+++ src/js/script.js
@@ -12,7 +12,18 @@ window.onload = function(){
     },3000)
 }
 
+//menu hamburguer 
 
+document.addEventListener('DOMContentLoaded' ,()=>{
+    const menuIcone= document.getElementById("menu-icone");
+    const navMenu = document.querySelector("nav");
+    if(menuIcone && navMenu){
+        menuIcone.onclick=()=>{
+            navMenu.classList.toggle("active");
+            menuIcone.classList.toggle("open");
+        }
+    }
+})
 
 
 
